6418482dcc4ca0dc4c229b74ee588611a72a6265
Fedor Filimonov <f.filimonov@gmail.com>
2021-02-13 17:34:18 +0300

    Completed lesson 2

---
+++ GBShop/App/BusinessLogic/Network/RequestFactory.swift
@@ -0,0 +1,31 @@
+//
+//  RequestFactory.swift
+//  StoreApp
+//
+//  Created by Федор Филимонов on 13.02.2021.
+//
+
+import Foundation
+import Alamofire
+
+class RequestFactory {
+    
+    func makeErrorParser() -> AbstractErrorParser {
+        return ErrorParser()
+    }
+    
+    lazy var commonSession: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpShouldSetCookies = false
+        configuration.headers = .default
+        let manager = Session(configuration: configuration)
+        return manager
+    }()
+    
+    let sessionQueue = DispatchQueue.global(qos: .utility)
+    
+    func makeAuthRequestFatory() -> AuthRequestFactory {
+        let errorParser = makeErrorParser()
+        return Auth(errorParser: errorParser, sessionManager: commonSession, queue: sessionQueue)
+    }
+}
---
+++ GBShop/App/Entity/LoginResult.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResult.swift
+//  StoreApp
+//
+//  Created by Федор Филимонов on 13.02.2021.
+//
+
+import Foundation
+
+struct LoginResult: Codable {
+    let result: Int
+    let user: User
+}
